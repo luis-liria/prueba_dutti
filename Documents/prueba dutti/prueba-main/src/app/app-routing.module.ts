956b78abf441fb0553eac30a4a830cc3433c8d78
Luis Liria <luisliria86@gmail.com>
2023-02-05 19:20:47 +0100

    update version angular 13

---
+++ Documents/prueba dutti/prueba-main/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+// Components
+import { LoginComponent } from './components/auth/pages/login/login.component';
+import { RegisterComponent } from './components/auth/pages/register/register.component';
+import { AuthGuard } from './components/auth/guards/auth.guard';
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./components/auth/auth.module').then( m => m.AuthModule )
+  },
+  { path: 'principal', loadChildren: () => import(`./components/principal/principal.module`).then(m => m.PrincipalModule),
+  canLoad: [AuthGuard],
+  canActivate: [ AuthGuard ]  }
+  // { path: 'ships', loadChildren: () => import(`./components/ships/ships.module`).then(m => m.ShipsModule) }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Documents/prueba dutti/prueba-main/src/app/components/auth/interfaces/auth.interfaces.ts
@@ -0,0 +1,6 @@
+export interface Auth {
+email: string
+first_name: string
+last_name : string
+username: string
+}
